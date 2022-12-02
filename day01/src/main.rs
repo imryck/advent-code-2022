65fbe4d9a202623e3d7294ea7a7bda2b0af14897
David MAURIN <d.maurin@groupeonepoint.com>
2022-12-02 16:16:04 +0100

    improves day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,36 +8,21 @@ struct Elf {
 
 fn main() {
     let cli_args = common::parse_cli_args();
-    let elves_snacks: Vec<Vec<i32>> = common::read_lines(&cli_args.input_path)
-        .unwrap()
-        .into_iter()
+    let mut elves: Vec<i64> = common::read_lines(&cli_args.input_path).unwrap().into_iter()
         .fold(Vec::new(), |mut acc, snack| {
             if acc.is_empty() || snack == "" {
                 acc.push(Vec::new());
-            }
-            if snack != "" {
-                acc.last_mut().unwrap().push(
-                    snack.to_string().parse::<i32>().unwrap()
-                );
+            } else {
+                acc.last_mut().unwrap().push(snack.as_str().parse::<i64>().unwrap());
             }
             acc
-        });
-    let mut elves: Vec<Elf> = elves_snacks.into_iter()
-        .map(|snacks| {
-            let calories = snacks.clone()
-                .into_iter()
-                .fold(0, |acc, s| acc + i64::from(s));
-            Elf {
-                snacks: snacks.clone(),
-                calories,
-            }
         })
-        .collect::<Vec<Elf>>();
-    elves.sort_by(|a, b| b.calories.cmp(&a.calories));
+        .into_iter()
+        .map(|snaks| snaks.into_iter().fold(0, |acc, s| acc + s))
+        .collect();
+    elves.sort();
+    elves.reverse();
 
-    println!("The elf with the most calories carrying {:#?} calories", elves.last().unwrap().calories);
-
-    let top_tree = &elves[0..3].into_iter()
-        .fold(0, |acc, elf| acc + i64::from(elf.calories));
-    println!("The top tree elves are {:#?}", top_tree);
+    println!("The elf with the most calories carrying {:#?} calories", elves.last().unwrap());
+    println!("The top tree elves are {:#?}", &elves[0..3].into_iter().fold(0, |acc, cal| acc + cal));
 }
